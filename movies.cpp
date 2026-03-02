44abafeaa6ad875abde223fb59bdcd166e68119d
codexr941 <xianze@ucsb.edu>
2026-03-01 20:30:27 -0800

    Update movies.cpp

--- movies.cpp
+++ movies.cpp
@@ -35,6 +35,69 @@ while (hi != movies.end() && hi->name.substr(0, prefix.size()) == prefix) {
     hi++;
 }
 
+void MovieDatabase::sortByName() {
+    sort(movies.begin(), movies.end(), nameLess);
+}
+
+void MovieDatabase::loadFromFile(const string& filename){
+  movies.clear();
+string line;
+while (getline(fin, line)) {
+ Movie m;
+        if (readOneMovie(line, m)) {
+            movies.push_back(m);
+        }
+    }
+fin.close();
+sortByName(); 
+}
+
+void MovieDatabase::printAllMovies() const{
+  for (int i = 0; i < (int)movies.size(); i++)
+    cout << movies[i].name << ", " << movies[i].rating << "\n";
+}
+
+bool MovieDatabase::queryPrefix(const string& prefix, string& bestLine) const {
+  auto lo = lower_bound(movies.begin(), movies.end(), prefix,
+        [](const Movie& m, const string& s) {
+            return m.name < s;
+        });
+if (lo == movies.end() || lo->name.substr(0, prefix.size()) != prefix) {
+return false;
+}
+auto hi = lo;
+    while (hi != movies.end() && hi->name.substr(0, prefix.size()) == prefix) {
+        hi++;
+    }
+vector<Movie> matches;
+    for (auto it = lo; it != hi; it++) matches.push_back(*it);
+   sort(matches.begin(), matches.end(), MovieDatabase::ratingDescNameAsc);
+for (int i = 0; i < (int)matches.size(); i++) {
+        cout << matches[i].name << ", " << matches[i].rating << "\n";
+    }
+    cout << "\n"; 
+ostringstream oss;
+    oss << fixed << setprecision(1);
+    oss << "Best movie with prefix " << prefix << " is: "
+        << matches[0].name << " with rating " << matches[0].rating;
+    bestLine = oss.str();
+
+    return true;
+}
+
+void MovieDatabase::processPrefixFile(const string& prefixFilename) const {
+  vector<string> bestLines;
+    string prefix;
+string bestLine;
+if (queryPrefix(prefix, bestLine)) {
+    bestLines.push_back(bestLine);
+}
+pin.close();
+for (int i = 0; i < bestLines.size(); i++) {
+    cout << bestLines[i] << "\n";
+}
+}
+  
 
 
 
