082b25ac1c787e6641a3ff3445e208e548a99cce
codexr941 <xianze@ucsb.edu>
2026-03-01 18:31:48 -0800

    Update movies.cpp

--- movies.cpp
+++ movies.cpp
@@ -1,1 +1,53 @@
 #include "movies.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+bool readOneMovie(const string& line, Movie& m){
+  int comma = (int)line.rfind(',');
+  if (comma == -1)
+    return false;
+  m.name = line.substr(0, comma);
+  string rateStr = line.substr(comma + 1);
+  m.rating = stod(rateStr);
+  return true;
+}
+
+bool nameLess(const Movie& a, const Movie& b) {
+    return a.name < b.name;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
